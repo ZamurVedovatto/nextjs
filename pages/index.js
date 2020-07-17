cb61c8b34294bc83e76e404bf441a9cbd45b36a0
Zamur Vedovatto <vedovatto@gmail.com>
2020-07-16 21:39:03 -0300

    getInitial props and fetchdata

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import './../styles/General.scss';
 
 import Layout from '../components/Layout';
-import Home from './Home';
 import fetch from 'isomorphic-unfetch';
+import Home from './home';
 
 const Index = (props) => {
   console.log(props);
-
+  const { posts } = props;
   return (
     <Layout>
       <Home />
+      <div className="container">
+        {
+          posts.map((post => (
+            <div key={post.id} className="card text-white bg-primary m-2">
+              <div className="card-header">{post.title}</div>
+              <div className="card-body">
+                <p className="card-text">{post.body}</p>
+              </div>
+            </div>
+          )))
+        }
+      </div>
     </Layout>
   )
 }
