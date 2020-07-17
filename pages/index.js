7e2cec7b15174676881d555d33be8d4008657c4a
Zamur Vedovatto <vedovatto@gmail.com>
2020-07-16 21:26:38 -0300

    ajustes

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,24 @@ import './../styles/General.scss';
 
 import Layout from '../components/Layout';
 import Home from './Home';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => {
+const Index = (props) => {
+  console.log(props);
+
   return (
     <Layout>
       <Home />
     </Layout>
   )
+}
+
+Index.getInitialProps = async ( context ) => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const results = await data.json();
+  return {
+    posts: results
+  }
 }
 
 export default Index;
